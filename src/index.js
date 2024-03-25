170347d88a3ddaf7b4bf94bd2076d4c9b7a86cbc
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-03-25 21:24:45 +0530

    uploading file using multer and cloudinary

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
+// Load environment variables from .env file
 // require("dotenv").config({path:"./env"})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import app from "./app.js"
+import app from "./app.js";
 
+// Load environment variables from .env file
 dotenv.config({
     path:"./env"
-})
+});
 
+// Connect to MongoDB
 connectDB()
 .then(()=>{
+    // Handle errors in the application
     app.on("error", (err) => {
-        console.log("ERROR :", err)
-        throw err
-    })
+        console.log("ERROR :", err);
+        throw err;
+    });
+
+    // Start the server
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`)
-    })
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    });
 })
 .catch((err) => {
     console.log("MONGODB CONNECTION FAILED",err);
-})
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+// Importing cloudinary library and renaming it to 'cloudinary'
+import { v2 as cloudinary } from "cloudinary";
+// Importing file system library
+import fs from "fs"
+
+// Configuring cloudinary with credentials from environment variables
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME , 
+    api_key: process.env.CLOUDINARY_API_KEY , 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// Function to upload file on Cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        // Checking if localFilePath is provided
+        if(!localFilePath) return null
+        // Uploading file to Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        // Logging success message
+        console.log("File uploaded successfully")
+        // Returning response from Cloudinary
+        return response
+    } catch (error) {
+        // If there's an error, delete the local file
+        fs.unlinkSync(localFilePath)
+        // Return null
+        return null
+    }
+}
+
+// Exporting the uploadOnCloudinary function
+export { uploadOnCloudinary }
