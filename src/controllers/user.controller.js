579ec851941d8934151a93757782fea7b981cd76
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-04-02 18:16:02 +0530

    auth middleware and login & logout controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,25 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import apiResponse from "../utils/apiResponse.js"
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave : false })
+
+        return {accessToken, refreshToken}
+
+    } 
+    
+    catch (error) {
+        throw new apiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
 const registerUser = asyncHandler (async(req,res) => {
     
     // res.status(200).json({
@@ -77,4 +96,81 @@ const registerUser = asyncHandler (async(req,res) => {
 
 })
 
-export {registerUser}
+const loginUser = asyncHandler (async(req,res) => {
+
+    // get user details from user ( req->body - data)
+    const {email, username, password} = req.body
+
+    // get access on username or email
+    if (!username || !email) throw new apiError(400, "Username or email is required")
+
+    // find the user based on username or email
+    const user = await User.findOne({
+        $or : [{username}, {email}]
+    })
+
+    if(!user) throw new apiError(404, "User doesnot exist!")
+
+    // check the password
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    
+    if(!isPasswordValid)  throw new apiError(401, "Password incorrect!")
+
+    // generate access and refresh token
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    // send tokens based on cookies
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken", refreshToken,options)
+    .json(
+        new apiResponse(
+            200,
+            {
+                user : loggedInUser , accessToken , refreshToken
+            }, 
+            "User logged in successfully!"
+        )
+    )
+})
+
+const logoutUser = asyncHandler (async(req,res) => {
+     await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new apiResponse(
+        200,
+        {
+
+        },
+        "User logged out successfully"
+    ))
+
+})
+
+export {registerUser, loginUser,logoutUser}
