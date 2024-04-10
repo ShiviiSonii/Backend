fe3a17a765271d63df87ab9873736ebf17f81f5e
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-04-10 16:52:11 +0530

    added new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    registerUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory
+ } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { refreshAccessToken } from "../controllers/user.controller.js";
 
 const router = Router()
 
@@ -27,5 +38,13 @@ router.route("/login").post(loginUser)
 // secured routes {user already needed}
 router.route("/logout").post(verifyJwt,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJwt,changeCurrentPassword)
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+router.route("/update-account-details").patch(verifyJwt,updateAccountDetails)
+router.route("/avatar-update").patch(verifyJwt,upload.single("avatar"),updateUserAvatar)
+router.route("/coverimage-update").patch(verifyJwt,upload.single("coverImage"),updateUserCoverImage)
+router.route("/channel/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/watch-history").get(verifyJwt,getUserWatchHistory)
 
 export default router
