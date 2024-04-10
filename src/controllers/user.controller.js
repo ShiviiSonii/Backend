682fad3d6500aae10726995283a38e695003a152
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-04-10 19:15:05 +0530

    fixed some controllers and added some new models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import apiError from "../utils/apiError.js";
 import {User} from "../models/user.model.js"
+import mongoose from "mongoose"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import apiResponse from "../utils/apiResponse.js"
 import jwt from "jsonwebtoken"
@@ -148,8 +149,8 @@ const logoutUser = asyncHandler (async(req,res) => {
      await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set:{
-                refreshToken: undefined
+            $unset:{
+                refreshToken: 1
             }
         },
         {
@@ -179,7 +180,7 @@ const logoutUser = asyncHandler (async(req,res) => {
 const refreshAccessToken = asyncHandler (async(req,res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new apiError(401,"Unauthorized request!")
     }
 
@@ -444,40 +445,42 @@ const getUserChannelProfile = asyncHandler (async(req,res) => {
     )
 })
 
-const getUserWatchHistory = asyncHandler (async(req,res) => {
+const getUserWatchHistory = asyncHandler(async(req, res) => {
     const user = await User.aggregate([
         {
-            $match : {
-                _id : new mongoose.Schema.Types.ObjectId(req.user._id)
-            },
-            $lookup : {
-                from : "videos",
-                localField : "watchHistory",
-                foreignField : "_id",
-                as : "watchHistory",
-                pipeline : [
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
                     {
-                        $lookup : {
-                            from : "users",
-                            localField : "owner",
-                            foreignField : "_id",
-                            as : "owner",
-                            pipeline : [
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
                                 {
-                                    $project : {
-                                        fullname : 1,
-                                        username : 1,
-                                        avatar : 1
-                                    }
-                                },
-                                {
-                                    $addFields : {
-                                        owner : {
-                                            $first : "$owner"
-                                        }
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
                                     }
                                 }
                             ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
                         }
                     }
                 ]
@@ -491,7 +494,7 @@ const getUserWatchHistory = asyncHandler (async(req,res) => {
         new apiResponse(
             200,
             user[0].watchHistory,
-            "Watch history fetched successfully!"
+            "Watch history fetched successfully"
         )
     )
 })
