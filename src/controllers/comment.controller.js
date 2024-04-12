77e196ef1dcd2bae8bfeba7a870bdad755783d65
ShiviiSonii <1111dhruvsoni@gmail.com>
2024-04-12 14:48:56 +0530

    comment controller added

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -14,14 +14,74 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const { content, video, owner } = req.body;
+    try {
+        const comment = await Comment.create({
+            content,
+            video,
+            owner
+        })
+    
+        if(!comment) throw new apiError(400,"Something went wrong while creating comment!")
+    
+        return res.status(200).json(
+            new apiResponse(
+                200,
+                comment,
+                "Comment added successfully!"
+            )
+        )
+    } catch (error) {
+        return res.status(500).json(
+            new apiError(500, "Internal Server Error")
+        );
+    }
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const { content, owner } = req.body;
+
+    if(!owner) throw new apiError(400,"Owner field is required!")
+
+    const comment = await Comment.findByIdAndUpdate(
+        req.comment?._id,
+        {
+            $set : {
+                content:content
+            }
+        },
+        { new : true}
+    )
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            comment,
+            "Comment updated successfully!"
+        )
+    )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {  owner } = req.body;
+
+    if(!owner) throw new apiError(400,"Owner field is required!")
+
+    const comment = await Comment.findByIdAndDelete(req.comment._id)
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            comment,
+            "Comment deleted successfully!"
+        )
+    )
 })
 
 export {
@@ -29,4 +89,4 @@ export {
     addComment, 
     updateComment,
      deleteComment
-    }
+}
